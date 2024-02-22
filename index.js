47991d95bab1c377c021c0442fb2fe11e1fbde09
AlexEngo <a.o.engovatov@yandex.ru>
2024-02-22 20:42:51 +0400

    return html page on get request

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const http = require("http");
 const chalk = require("chalk");
-const PORT = 3000;
+const fs = require("fs/promises");
+const path = require("path");
 
-const server = http.createServer((req, res) => {
-    console.log("Method:", req.method);
-    console.log("url:", req.url);
-    console.log("Server");
-    res.end("Hello from server!");
+const PORT = 3000;
+const basePath = path.join(__dirname, "pages");
+
+const server = http.createServer(async (req, res) => {
+    if (req.method === "GET") {
+        const content = await fs.readFile(path.join(basePath, "index.html"));
+        res.setHeader("Content-Type", "text/html");
+        res.writeHead(200);
+        res.end(content);
+    }
 });
 
 server.listen(PORT, () => {
