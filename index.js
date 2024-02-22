7f43e7c6629b82282beed9f02b22a11688f3a745
AlexEngo <a.o.engovatov@yandex.ru>
2024-02-22 20:05:17 +0400

    init project

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const chalk = require("chalk");
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    console.log("Server");
+    res.end("Hello from server!");
+});
+
+server.listen(PORT, () => {
+    console.log(chalk.green(`Server has been started on port ${PORT}`));
+});
