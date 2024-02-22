8a740da2b11eacceb280cb50e51eed1a1fe96406
AlexEngo <a.o.engovatov@yandex.ru>
2024-02-22 21:34:28 +0400

    refactoring by express

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const express = require("express");
 const chalk = require("chalk");
 const fs = require("fs/promises");
 const path = require("path");
@@ -7,26 +8,20 @@ const { addNote } = require("./notes.controller");
 const PORT = 3000;
 const basePath = path.join(__dirname, "pages");
 
-const server = http.createServer(async (req, res) => {
-    if (req.method === "GET") {
-        const content = await fs.readFile(path.join(basePath, "index.html"));
-        res.setHeader("Content-Type", "text/html");
-        res.writeHead(200);
-        res.end(content);
-    } else if (req.method === "POST") {
-        res.writeHead(200, {
-            "Content-Type": "text/plain",
-        });
-        const body = [];
-        req.on("data", (data) => {
-            body.push(Buffer.from(data));
-        });
-        req.on("end", () => {
-            const title = body.toString().split("=")[1].replaceAll("+", " ");
-            addNote(title);
-            res.end(`Title = ${title}`);
-        });
-    }
+const server = express();
+server.use(
+    express.urlencoded({
+        extanded: true,
+    })
+);
+
+server.get("/", (req, res) => {
+    res.sendFile(path.join(basePath, "index.html"));
+});
+
+server.post("/", async (req, res) => {
+    await addNote(req.body.title);
+    res.sendFile(path.join(basePath, "index.html"));
 });
 
 server.listen(PORT, () => {
